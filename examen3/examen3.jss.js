383295ddd9b2c1349bf01ad886b54a5ad510c992
alexalmanza2333 <161179411+alexalmanza2333@users.noreply.github.com>
2024-05-17 07:44:24 -0600

    Add files via upload

---
+++ examen3/examen3.jss.js
@@ -0,0 +1,15 @@
+</script>
+</head>
+<body>
+    <h1>Verificador de Palíndromos</h1>
+    <form>
+        <!-- Cuadro de texto para ingresar la frase -->
+        <label for="frase">Introduce una frase:</label>
+        <input type="text" id="frase" name="frase">
+        <!-- Botón para evaluar la frase -->
+        <button type="button" onclick="evaluarPalindromo()">Evaluar</button>
+    </form>
+    <!-- Párrafo para mostrar el resultado -->
+    <p id="resultado"></p>
+</body>
+</html>
